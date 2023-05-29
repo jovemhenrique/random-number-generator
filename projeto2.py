d8d789e1e5efa2d6991d3f5499e3b961648fdfe0
Miguel Pedrosa <93871576+migueljcpedrosa@users.noreply.github.com>
2023-05-29 20:29:03 +0100

    Added 🍌

--- projeto2.py
+++ projeto2.py
@@ -1,4 +1,21 @@
 while True:
+    word = input("Enter a word: ")
+
+    if word.lower() == "banana":
+        print("You found the secret word!")
+        break
+
+    else:
+        print("Oops! That's not the secret word.")
+        print("Here's a banana joke for you:")
+        print("Why did the banana go to the doctor?")
+        print("Because it wasn't peeling well!")
+        print()
+
+print("Congratulations! You won!")
+
+
+while True:
 	string = input("Palavra??")
 
 	if string == "pngbaby":
@@ -8,3 +25,5 @@ while True:
 	else:
 		for i in range(0,20):
 			print("Vai á merda")
+			
+			
